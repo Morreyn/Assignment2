331bd3512e78eb1b535d363a9f832166d44125b0
Misty Morreyn <misty.morreyn@gmail.com>
2015-09-19 02:23:59 -0700

    Progressed through to Part 5 of Assignment 2.

--- Assignment 2.js
+++ Assignment 2.js
@@ -4,7 +4,7 @@
     CLASS: COMM644 FALL 2015
 */
 
-// ASSIGNMENT 2 PART 1:
+// PART 1:
 
 /* 
 // STEP 1. Create an application that prompts the user for their name. Then, find the length of characters in the person’s name.  Use the alert method to display the result.
@@ -81,7 +81,7 @@ var str = "the quick brown fox jumps over the lazy dog";
 alert(str.replace("t", "T"));
 */
 
-// ASSIGNMENT 2 PART 2:
+// PART 2:
 
 /*
 // STEP 1: 1. Create an application that prompts the user for a number. Regardless of whether they enter a negative or positive number, make sure to display the positive version of that number in the console window. If I enter -15 in the prompt and -15 is displayed in the console window, you did this one wrong.
@@ -294,8 +294,149 @@ switch (dDay) {
 }
 */
 
+// PART 3:
 
+/*
 // STEP 12. Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the console window.
+
+var int1 = prompt("Type a number.");
+var int2 = prompt("Type another number.");
+
+if (int1 > int2) {
+    console.log(int1);
+} else {
+    console.log(int2);
+};
+*/
+
+/*
+// STEP 13. Create an application that records the marks for the following five students. Then, these marks are used to determine the corresponding grade. All 5 students and their grades should be displayed within the console window. 
+
+//Student Name/Marks  Range/Grade
+//Ursula/80           <60/F
+//Paul/77             <70/D
+//Henry/88            <80/C
+//Tabitha/95          <90/B
+//Lucy/68             <100/A
+
+
+var ursulaGrade = 80, paulGrade = 77, henryGrade = 88, tabithaGrade = 95, lucyGrade = 68;
+    
+function letterGrade(x, name) {
+    if (100 > x && x >= 90) {
+        console.log(name + " : A");
+    } else if (90 > x && x >= 80) {
+        console.log(name + " : B");
+    } else if (80 > x && x >= 70) {
+        console.log(name + " : C");
+    } else if (70 > x & x >= 60) {
+        console.log(name + " : D");
+    } else if (60 > x) {
+        console.log(name + " : F");
+    } else {
+        console.log(name + " failed hard.");
+    }
+}
+
+letterGrade(ursulaGrade, "Ursula");
+letterGrade(paulGrade, "Paul");
+letterGrade(henryGrade, "Henry");
+letterGrade(tabithaGrade, "Tabitha");
+letterGrade(lucyGrade, "Lucy");
+
+//Note: Wanted to do this with an student Object and the if/else function nexted within a while loop that looped through the object, but couldn't get it to work. Ask instructor.
+*/
+
+/*
+// STEP 14. Create a JavaScript for loop that iterates from 1 to 15. Each iteration should check if the current number is odd or even, and display a message within the console window.
+
+for (var i = 1; i <= 15; i++) {
+    if (i % 2 == 0) {
+        console.log(i + " is even.");
+    } else {
+        console.log(i + " is odd.");
+    }
+}
+*/
+
+/*
+// STEP 15. Create an application which iterates numbers from 1 to 100. For multiples of 3, print "Fizz" instead of the number and for multiples of 5, print "Buzz" instead of the number. For numbers which are multiples of both 3 and 5 print "FizzBuzz". This is a VERY common JavaScript interview question and you should know how to do this. You will need to take advantage of the modulus operator to accomplish this task.
+
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+*/
+
+// PART 4: "Hitchhiker’s Guide to the Galaxy Game"
+
+/*
+// STEP 1. Begin your application by asking the user if they’re ready to play your game. If they are, display the message “Awesome, our hero is waiting!”  If they’re not, display the message “Too bad, we’re going to play anyway because our hero desperately needs your help!” You will need to use the confirm box for this one and check for the Boolean result of clicking OK or Cancel.
+
+var readyMsg = confirm("Are you ready to play my Hitchhiker’s Guide to the Galaxy game?");
+
+if (readyMsg === true) {
+    alert("Awesome, our hero is waiting!");
+} else {
+    alert("Too bad, we’re going to play anyway because our hero desperately needs your help!");
+}
+
+// STEP 2. Now, alert the user to the following scenario: “You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall…”
+
+alert("You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall…");
+
+// STEP 3. Then prompt the user with the following message: “Which direction would you like to head (please enter forward, left, or right).”
+
+var choice = prompt("Which direction would you like to head (please enter forward, left, or right).");
+
+// STEP 4. Use a switch statement to check for the literal string values forward, left, and right. If they choose forward, display the message “You walk about 100 yards and safely make your way out of the cave.” If they choose left, display the message “Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown.” If they choose right, display the message “You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever.” If the user chooses anything other than those three values, display the message “The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option….loser.”
+
+switch (choice.toLowerCase()) {
+    case "forward":
+        alert("You walk about 100 yards and safely make your way out of the cave.");
+        break;
+    case "left":
+        alert("Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown.");
+        break;
+    case "right":
+        alert("You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever.");
+        break;
+    default:
+        alert("The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option….loser.");
+}
+
+// STEP 5. Finally, ask the user to rate your game on a scale of 1 to 10. If the feedback score is between 6 and 10, display the message “Thank you, we will continue to make improvements to the game!” If the feedback score is between 1 and 5, display the message “Whatever, you weren’t very good at this game anyway!” 
+// STEP 6. 6. Before you evaluate the number that the user has entered, check to see if the user entered a valid numeric value between 1 and 10. If they don’t, default the value to 10 and proceed with the evaluation.
+
+var rating = prompt("Please rate my game from 1 to 10! 1 means it sucks, and 10 means it's awesomesauce.");
+
+if (rating.match(/\d/)) {
+    if (1 <= rating && rating <= 5) {
+        alert("Whatever, you weren’t very good at this game anyway!");
+    } else if (6 <= rating && rating <= 10) {
+        alert("Thank you, we will continue to make improvements to the game!");
+    } else {
+        alert("I'm gonna assume you meant 10.");
+    }
+} else {
+    rating = 10;
+    alert("Thank you, we will continue to make improvements to the game!");
+}
+*/
+
+// PART 5:
+
+// STEP 1. Begin your application by creating a variable called coinFlip and set it equal to a random number. You will have to use a Math method to get this number.
+
+
+
 
 
 
